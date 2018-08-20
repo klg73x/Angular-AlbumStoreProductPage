b260b6eb20d7d8c79fa7c8ac4b0cf17ec37ecd5f
Kevin Gagnon <klg73@outlook.com>
2018-08-20 08:37:37 -0400

    Album List Completed

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,9 +10,10 @@ import { ProductService } from '../product.service'
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe(response => this.products = response);
   }
 
 }
